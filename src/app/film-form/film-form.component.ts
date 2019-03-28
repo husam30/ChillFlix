3d41987ff6b4f1dcd68af5072687c4f9ce5de1c8
Sam10715 <45232970+Sam10715@users.noreply.github.com>
2019-03-28 16:06:15 +0100

    Samer_changes (#8)

--- src/app/film-form/film-form.component.ts
+++ src/app/film-form/film-form.component.ts
@@ -4,6 +4,8 @@ import { Show } from '../shared/model/show.model';
 import { CategoryService } from '../shared/services/category.service';
 import { Category } from '../shared/model/category.model';
 import { ShowService } from '../shared/services/show.service';
+import { ShowForm1 } from './editshows.form';
+import { CreateShow } from '../shared/model/create-show.model';
 
 @Component({
   selector: 'app-film-form',
@@ -11,18 +13,26 @@ import { ShowService } from '../shared/services/show.service';
 })
 export class FilmFormComponent implements OnInit {
   public categories: Category[];
+  public shows: Show[];
   public newForm: ShowForm = new ShowForm();
+  public newForm1: ShowForm1 = new ShowForm1();
+
   constructor(
     private showService: ShowService,
     private categoryservice: CategoryService
   ) {}
   public onFormSubmit() {
-    const show1: Show = this.newForm.getModel();
+    const show1: CreateShow = this.newForm.getModel();
     this.showService.saveShow(show1).subscribe(() => {});
+  }
+  public onFormSubmit1() {
+    const show1: Show = this.newForm1.getModel();
+    this.showService.editShow(show1).subscribe(() => {});
   }
   ngOnInit() {
     this.categoryservice
       .getAllcategories()
       .subscribe(categories => (this.categories = categories));
+    this.showService.getAllShows().subscribe(shows => (this.shows = shows));
   }
 }
--- src/app/shared/services/show.service.ts
+++ src/app/shared/services/show.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Category } from '../model/category.model';
 import { Show } from '../model/show.model';
+import { CreateShow } from '../model/create-show.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,20 @@ import { Show } from '../model/show.model';
 export class ShowService {
   private readonly endpiont1 = 'http://localhost:8080/get-one-show';
   private readonly endpiont2 = 'http://localhost:8080/save-show';
+  private readonly endpiont3 = 'http://localhost:8080/get-show';
+  private readonly endpiont4 = 'http://localhost:8080/edit-show';
   constructor(private readonly http: HttpClient) {}
 
   public getOneShow(id: number): Observable<Show> {
     return this.http.get<Show>(this.endpiont1 + '/' + id);
   }
-  public saveShow(show: Show): Observable<void> {
+  public saveShow(show: CreateShow): Observable<void> {
     return this.http.post<void>(this.endpiont2, show);
+  }
+  public getAllShows(): Observable<Show[]> {
+    return this.http.get<Show[]>(this.endpiont3);
+  }
+  public editShow(show: Show): Observable<void> {
+    return this.http.post<void>(this.endpiont4, show);
   }
 }
